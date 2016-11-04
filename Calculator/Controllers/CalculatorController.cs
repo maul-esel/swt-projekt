724396b1049519960430432f62bae7b5d87026b3
maul.esel <maul.esel@go4more.de>
2016-11-04 12:56:59 +0100

    add CalculatorController

---
+++ Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices.ComTypes;
+using System.Web.Http;
+
+namespace Calculator.Controllers
+{
+	[RoutePrefix("api/calculator")]
+    public class CalculatorController : ApiController
+    {
+	    [Route("add/{n1}/{n2}")]
+	    public int Add(int n1, int n2 )
+	    {
+		    return n1 + n2;
+	    }
+
+	    [Route("multiply/{n1}/{n2}")]
+	    public int Multiply(int n1, int n2)
+	    {
+		    return n1 * n2;
+	    }
+    }
+}
