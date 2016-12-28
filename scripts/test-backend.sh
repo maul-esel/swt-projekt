87e314405b063abaf3917f63c32b98f637af5801
maul.esel <maul.esel@go4more.de>
2016-12-28 16:28:16 +0100

    include test runs (backend) in CI

---
+++ scripts/test-backend.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ "$DOTNETCORE" != "1" ]]; then
+  echo "No backend test"
+  exit
+fi
+
+set -e
+XUNIT_RUNNER="${TRAVIS_BUILD_DIR}/testrunner/xunit.runner.console.2.1.0/tools/xunit.console"
+
+echo "======= Testing Common   ======="
+cd "${TRAVIS_BUILD_DIR}/Lingvo/Solution/tests/Common.Tests/bin/Debug/netcoreapp1.0"
+mono "${XUNIT_RUNNER}" Lingvo.Common.Tests.dll
+
+echo "======= Testing Backend  ======="
+cd "${TRAVIS_BUILD_DIR}/Lingvo/Solution/tests/Backend.Tests/bin/Debug/netcoreapp1.0"
+mono "${XUNIT_RUNNER}" Lingvo.Backend.Tests.dll
