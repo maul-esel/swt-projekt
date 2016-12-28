4eb3c94ee2439a29840d0d07495843713f3e0f83
maul.esel <maul.esel@go4more.de>
2016-12-28 17:48:38 +0100

    dotnet test must be run from project root

--- scripts/test-backend.sh
+++ scripts/test-backend.sh
@@ -9,9 +9,9 @@ set -e
 XUNIT_RUNNER="${HOME}/.nuget/packages/xunit.runner.console/2.1.0/tools/xunit.console"
 
 echo "======= Testing Common   ======="
-cd "${TRAVIS_BUILD_DIR}/Lingvo/Solution/tests/Common.Tests/bin/Debug/netcoreapp1.0"
+cd "${TRAVIS_BUILD_DIR}/Lingvo/Solution/tests/Common.Tests"
 dotnet test
 
 echo "======= Testing Backend  ======="
-cd "${TRAVIS_BUILD_DIR}/Lingvo/Solution/tests/Backend.Tests/bin/Debug/netcoreapp1.0"
+cd "${TRAVIS_BUILD_DIR}/Lingvo/Solution/tests/Backend.Tests"
 dotnet test
