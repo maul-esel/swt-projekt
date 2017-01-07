0aedc168a062eb4e942ed356d7faf62c57cca9ee
Katja <katjal.email@web.de>
2017-01-07 13:49:23 +0100

    page und recording erstellt

---
+++ Lingvo/Solution/src/Common/Lingvo.Common.Entities/Page.cs
@@ -0,0 +1,78 @@
+﻿using System;
+namespace Lingvo.Common
+{
+	public class Page
+	{
+		private int number;
+		private String description;
+
+		private Recording teacherTrack;
+		private Recording studentTrack;
+
+		public int Number
+		{
+			get
+			{
+				return number;
+			}
+			set
+			{
+				number = value;
+			}
+		}
+
+		public String Description
+		{
+			get
+			{
+				return description;
+			}
+			set
+			{
+				description = value;
+			}
+		}
+
+		public bool Edited
+		{
+			get
+			{
+				return studentTrack != null;
+			}
+		}
+
+		public Recording TeacherTrack
+		{
+			get
+			{
+				return teacherTrack;
+			}
+			set
+			{
+				teacherTrack = value;
+			}
+		}
+
+		public Recording StudentTrack
+		{
+			get
+			{
+				return studentTrack;
+			}
+			set
+			{
+				studentTrack = value;
+			}
+		}
+
+
+		public Page()
+		{
+		}
+
+		public void DeleteStudentRecording()
+		{
+			studentTrack = null;
+		}
+	}
+}
---
+++ Lingvo/Solution/src/Common/Lingvo.Common.Entities/Recording.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Lingvo.Common
+{
+	public class Recording
+	{
+		private TimeSpan length;
+		private DateTime creationTime;
+
+		public TimeSpan Length
+		{
+			get
+			{
+				return length;
+			}
+			set
+			{
+				length = value;
+			}
+		}
+
+		public Recording()
+		{
+			creationTime = DateTime.Now;
+		}
+	}
+}
