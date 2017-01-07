0db9bea49ae8d03fdd5ddbf7f48d7709dc977b31
Katja <katjal.email@web.de>
2017-01-07 15:48:03 +0100

    vorher was vergessen

--- Lingvo/Solution/src/Backend/Controllers/EditorController.cs
+++ Lingvo/Solution/src/Backend/Controllers/EditorController.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using Lingvo.Common;
 namespace Lingvo.Backend
 {
 	public class EditorController
 	{
+		private IRecorder recorder;
+		private IPlayer player;
+
+		private Page CurrentPage
+		{
+			get;
+			set;
+		}
+
+		private Workbook CurrentWorkbook
+		{
+			get;
+			set;
+		}
+
 		public EditorController()
 		{
+		}
+
+		public void createPage(Workbook workbook, int pageNumber)
+		{
+			CurrentPage = new Page();
 		}
 	}
 }
