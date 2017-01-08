2079c04c79ddbd63eb3e538c3fe8da5fc9698a37
maul.esel <maul.esel@go4more.de>
2017-01-08 10:13:00 +0100

    fix mysql syntax

--- Lingvo/SQL/server.sql
+++ Lingvo/SQL/server.sql
@@ -4,13 +4,13 @@ CREATE TABLE Workbooks (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title TEXT NOT NULL,
   subtitle TEXT,
-  lastModified TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP, /* client: sqlite syntax */
+  lastModified TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
   published BOOLEAN NOT NULL
 );
 
 CREATE TABLE Recordings (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  creationTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, /* client: sqlite syntax */
+  creationTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   length INT NOT NULL,
   localPath TEXT NOT NULL
 );
@@ -19,8 +19,11 @@ CREATE TABLE Pages (
   workbookID INT REFERENCES Workbooks,
   number INT,
   description TEXT NOT NULL,
-  teacherTrack INT NOT NULL REFERENCES Recordings,
-  studentTrack INT REFERENCES Recordings, /* always NULL on server */
+  teacherTrack INT NOT NULL,
+  studentTrack INT, /* always NULL on server */
   
-  PRIMARY KEY (workbookID, number)
+  PRIMARY KEY (workbookID, number),
+  FOREIGN KEY wbid(workbookID) REFERENCES Recordings(id),
+  FOREIGN KEY ttfk(teacherTrack) REFERENCES Recordings(id),
+  FOREIGN KEY stfk(studentTrack) REFERENCES Recordings(id)
 );
