cbf1c4538e8363151a7b20e588c2a9821744b87e
maul.esel <maul.esel@go4more.de>
2017-01-08 13:05:32 +0100

    server: cleanup database access code

--- Lingvo/Solution/src/Backend/Controllers/AppController.cs
+++ Lingvo/Solution/src/Backend/Controllers/AppController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using LinqToDB;
-using LinqToDB.Data;
-using LinqToDB.DataProvider.MySql;
 
 namespace Lingvo.Backend.Controllers
 {
@@ -12,26 +9,10 @@ namespace Lingvo.Backend.Controllers
 	[Route("api/app")]
 	public class AppController : Controller
     {
-		private DataConnection _connection;
-
-		private DataConnection Connection
-		{
-			get
-			{
-				if (_connection == null)
-				{
-					_connection = MySqlTools.CreateDataConnection("Server=127.0.0.1;Port=3306;Database=lingvo;Uid=root;Pwd=password;charset=utf8;");
-					_connection.MappingSchema.SetConverter<int, TimeSpan>(ms => TimeSpan.FromMilliseconds(ms));
-				}
-				return _connection;
-			}
-		}
-
 		[Route("workbooks"), HttpGet]
 		public IActionResult GetWorkbooks()
 		{
-			var workbooks = Connection.GetTable<Workbook>().LoadWith(w => w.Pages);
-			return Json(from w in workbooks
+			return Json(from w in Database.Workbooks
 						where w.IsPublished
 						select new { w.Id, w.Title, w.Subtitle, w.LastModified, w.TotalPages });
 		}
@@ -39,8 +20,7 @@ namespace Lingvo.Backend.Controllers
 		[Route("workbooks/{workbookId}/pages")]
 		public IActionResult GetPages(int workbookId)
 		{
-			var pages = Connection.GetTable<Page>();
-			return Json(from p in pages
+			return Json(from p in Database.Pages
 						where p.workbookId == workbookId
 						select new { p.workbookId, p.Number, p.Description });
 		}
@@ -48,8 +28,7 @@ namespace Lingvo.Backend.Controllers
 		[Route("workbooks/{workbookId}/pages/{pageNumber}")]
 		public IActionResult GetTeacherTrack(int workbookId, int pageNumber)
 		{
-			var pages = Connection.GetTable<Page>().LoadWith(p => p.TeacherTrack);
-			var page = pages.FirstOrDefault(p => p.workbookId == workbookId && p.Number == pageNumber);
+			var page = Database.Pages.Find(workbookId, pageNumber);
 			if (page == null)
 				return NotFound();
 
---
+++ Lingvo/Solution/src/Backend/Database.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LinqToDB;
+using LinqToDB.Data;
+using LinqToDB.DataProvider.MySql;
+
+namespace Lingvo.Backend
+{
+	using Common;
+
+    public static class Database
+    {
+		internal static string ConnectionString { get; set; }
+
+		private static DataConnection _connection;
+
+		private static DataConnection Connection
+		{
+			get
+			{
+				if (_connection == null)
+				{
+					_connection = MySqlTools.CreateDataConnection(ConnectionString);
+					_connection.MappingSchema.SetConverter<int, TimeSpan>(ms => TimeSpan.FromMilliseconds(ms));
+				}
+				return _connection;
+			}
+		}
+
+		public static ITable<Recording> Recordings => Connection.GetTable<Recording>();
+		public static ITable<Page> Pages => Connection.GetTable<Page>()
+			.LoadWith(p => p.Workbook)
+			.LoadWith(p => p.TeacherTrack);
+		public static ITable<Workbook> Workbooks => Connection.GetTable<Workbook>()
+			.LoadWith(w => w.Pages);
+
+		public static Page Find(this ITable<Page> pages, int workbookId, int pageNumber)
+		{
+			return pages.FirstOrDefault(page => page.workbookId == workbookId && page.Number == pageNumber);
+		}
+    }
+}
