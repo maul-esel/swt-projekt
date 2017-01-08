48b2212ef85b7fdfe6d419835173686b74e8b190
Jan Göbel <jan.goebel.home@gmail.com>
2017-01-08 14:35:01 +0100

    add SingleProgressView

---
+++ Lingvo/Solution/src/MobileApp.iOS/Views/SingleProgressView.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UIKit;
+using CoreGraphics;
+using Foundation;
+using CoreAnimation;
+
+namespace Lingvo.MobileApp.iOS
+{
+	public class SingleProgressView : CircleProgressBar
+	{
+
+		UILabel label;
+		private string labelText = "100%";
+		public string LabelText
+		{
+			get
+			{
+				return labelText;
+			}
+			set
+			{
+				labelText = value;
+				label.Text = labelText;
+			}
+		}
+
+
+		public SingleProgressView(CGRect frame) : base(frame)
+		{
+			setupViews();
+		}
+
+
+
+		protected override void setupViews()
+		{
+			base.setupViews();
+
+			label = new UILabel(new CGRect(0, 0, Frame.Width / 4, 40))
+			{
+				Text = "100%",
+				TextAlignment = UITextAlignment.Center,
+				Font = UIFont.SystemFontOfSize(28)
+			};
+
+			runOnMainThread(new Action(() =>
+			{
+				CenterView = label;
+			}));
+
+
+		}
+	}
+}
