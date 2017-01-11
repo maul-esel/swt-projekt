0544d2a3dd9299edc8d0bce3fa0ed8f55776d3c6
Katja <katjal.email@web.de>
2017-01-11 15:59:13 +0100

    sql file for dummy server data added [CI skip]

---
+++ Lingvo/SQL/DummyDataForServer.sql
@@ -0,0 +1,19 @@
+DELETE FROM Workbooks;
+DELETE FROM Pages;
+DELETE FROM Recordings;
+
+INSERT INTO Workbooks(id, title, subtitle, totalPages, published) VALUES
+(1, 'Deutschkurs für Asylbewerber', 'Thannhauser Modell', 2, 1),
+(2, 'Willkommen! Die deutsche Sprache - erste Schritte', 'Flüchtlingshilfe München e.V.', 2, 1);
+
+INSERT INTO Recordings (id, length, localPath) VALUES
+(1, 127000, 'audios/diktat_seite_01.mp3'),
+(2, 462000, 'audios/diktat_seite_02.mp3'),
+(3, 127000, 'audios/diktat_seite_03.mp3'),
+(4, 368000, 'audios/diktat_seite_04.mp3');
+
+INSERT INTO Pages(workbookID, number, description, teacherTrack) VALUES
+(1, 1, 'Begrüßung, Vorstellung und Familie', 1),
+(1, 2, 'Wie geht es Ihnen?', 2),
+(2, 1, 'Das Alphabet', 3),
+(2, 2, 'Erste Gespräche', 4);
