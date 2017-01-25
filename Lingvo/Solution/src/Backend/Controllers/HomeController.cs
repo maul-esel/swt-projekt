d8e5723de6b36ac08943339783f16e0c6cc7bbc9
Ralph Reithmeier <reithmeierr@gmail.com>
2017-01-25 17:20:38 +0100

    Fileupload Backend - Raw File

--- Lingvo/Solution/src/Backend/Controllers/HomeController.cs
+++ Lingvo/Solution/src/Backend/Controllers/HomeController.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
+using Lingvo.Common.Entities;
+using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Lingvo.Backend.Controllers
 {
     public class HomeController : Controller
     {
+
+		private IHostingEnvironment environment;
+
+		public HomeController(IHostingEnvironment environment)
+		{
+			this.environment = environment;
+		}
+
         public IActionResult Index()
         {
             return View();
@@ -44,5 +56,24 @@ namespace Lingvo.Backend.Controllers
 		{
 			return true;
 		}
-    }
+    
+		[HttpPost]
+		public async Task<IActionResult> UploadFile(IFormFile file)
+		{
+
+			var uploads = Path.Combine(environment.WebRootPath, "uploaded");
+			var filePath = Path.Combine(uploads, file.FileName);
+
+			Console.WriteLine(filePath);
+			Console.WriteLine(file);
+			//TODO: Create Recording and save it to database
+			//TODO: Get sound duration somehow from file => crazy magic!
+
+			var stream = new FileStream(filePath, FileMode.Create);
+			await file.CopyToAsync(stream);
+			stream.Dispose();
+
+			return View("Workbook");
+		}
+	}
 }
