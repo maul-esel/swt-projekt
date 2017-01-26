bde0536e50fe16392e1ef3206b1fd503fd7d8fbd
maul.esel <maul.esel@go4more.de>
2017-01-26 17:34:51 +0100

    Common.Tests no longer exists - don't build it

--- scripts/test-backend.sh
+++ scripts/test-backend.sh
@@ -8,10 +8,6 @@ fi
 set -e
 XUNIT_RUNNER="${HOME}/.nuget/packages/xunit.runner.console/2.1.0/tools/xunit.console"
 
-echo "======= Testing Common   ======="
-cd "${TRAVIS_BUILD_DIR}/Lingvo/Solution/tests/Common.Tests"
-dotnet test
-
 echo "======= Testing Backend  ======="
 cd "${TRAVIS_BUILD_DIR}/Lingvo/Solution/tests/Backend.Tests"
 dotnet test
