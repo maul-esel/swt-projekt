deb6304b15d7d0bc2b3050de3921a084e4340cbf
Ralph Reithmeier <reithmeierr@gmail.com>
2017-02-11 15:03:26 +0100

    JS ausgelagert, timeCode added

---
+++ Lingvo/Solution/src/Backend/wwwroot/js/recorder/webrecorder.js
@@ -0,0 +1,84 @@
+
+  var elapsed_time = 0;
+  var elapsed_time_display;
+  var audio_context;
+  var recorder;
+  var current_recording;
+  var recording = 0;
+  
+  function startRecording(button) {
+    recording = recording + 1;
+    recorder.clear();
+    recorder && recorder.record();
+    button.disabled = true;
+    button.nextElementSibling.disabled = false;
+    elapsed_time_display = setInterval(displayElapsedTime,1000);
+  }
+  
+  function stopRecording(button) {
+    recorder && recorder.stop();
+    button.disabled = true;
+    button.previousElementSibling.disabled = false;
+    clearInterval(elapsed_time_display);
+    prepareRecording();
+  }
+  
+  function displayElapsedTime() {
+    
+    $("#seconds").html(padTimeCode(++elapsed_time%60));
+    $("#minutes").html(padTimeCode(parseInt(elapsed_time/60,10)));
+    
+  }
+
+  function resetElapsedTime() {
+    elapsed_time = 0;
+    displayElapsedTime();
+  }
+
+  function prepareRecording() {
+    recorder.exportMP3(function(blob){
+        current_recording = blob;
+        setNewRecordingDisplay(URL.createObjectURL(blob));
+      //Functions for displaying the current recording could be called here
+      //Playback function could be set up here
+    });
+  }
+
+  function sendBlobToServer() {
+        if(current_recording == null){
+          return;}
+       
+        var form = $("#pageForm")[0];
+        var action = form.getAttribute("action")
+
+        var formData = new FormData(form);
+        formData.append("RecordedFile", current_recording);
+        var request = new XMLHttpRequest();
+
+        request.open("POST", action); //Not sure which method exactly should be called? Do we have to call a method???
+        request.send(formData);
+  }
+  
+  window.onload = function init() {
+    audioRecorder.requestDevice(function(recorderObject){
+      recorder = recorderObject;
+    });
+    };
+
+    function setNewRecordingDisplay(blobUrl)
+    {
+        var now = new Date();
+        $("#newRecordingName").text("Aufgenommen um " + now.getHours() + ":" + now.getMinutes() + " Uhr");
+        $("#newRecordingAudio").attr("src", blobUrl)
+        $("#noNewRecordingWarning").hide();
+        $("#newRecording").show();
+    }
+
+    function setUploadedFileDisplay()
+    {
+
+    }
+
+    function padTimeCode ( val ) {
+     return val > 9 ? val : "0" + val; 
+   }
