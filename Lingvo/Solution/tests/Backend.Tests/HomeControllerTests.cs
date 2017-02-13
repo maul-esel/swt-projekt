2fa6304b32aa6d67bc759fa593bebf07ed227058
Katja <katjal.email@web.de>
2017-02-13 20:55:55 +0100

    finished home controller testing

--- Lingvo/Solution/tests/Backend.Tests/HomeControllerTests.cs
+++ Lingvo/Solution/tests/Backend.Tests/HomeControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 using Lingvo.Common.Entities;
 
@@ -10,6 +11,7 @@ using Microsoft.EntityFrameworkCore;
 
 using Microsoft.AspNetCore.Mvc;
 using Lingvo.Backend.Controllers;
+using Lingvo.Backend.ViewModels;
 
 namespace Lingvo.Backend.Tests
 {
@@ -83,6 +85,85 @@ namespace Lingvo.Backend.Tests
 			Assert.Null(db.Pages.Find(1));
 			Assert.Null(db.Recordings.Find(1));
 
+		}
+
+		[Fact]
+		public async Task TestEditPage()
+		{
+			var controller = new HomeController(null);
+			var db = DatabaseService.Connect(TestsFixture.ConnectionString);
+
+			var result = await controller.EditPage(db, new StorageMock(), 1);
+
+			var viewResult = Assert.IsType<ViewResult>(result);
+			var model = Assert.IsType<PageModel>(viewResult.ViewData.Model);
+			Assert.Equal(1, model.Page.Id);
+			Assert.Equal(1, model.Workbook.Id);
+			Assert.Equal("abc", model.CurrentRecordingUrl);
+		}
+
+		[Fact]
+		public void TestCreateWorkbook()
+		{
+			var controller = new HomeController(null);
+			var db = DatabaseService.Connect(TestsFixture.ConnectionString);
+
+			var result = controller.CreateWorkbook(db, "NeuesWorkbook", "Nur zum Testen");
+
+			var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+			Assert.Equal("Index", redirectToActionResult.ActionName);
+
+			Assert.Equal(3, db.Workbooks.Count());
+			Assert.Equal("NeuesWorkbook", db.Workbooks.Last().Title);
+			Assert.Equal("Nur zum Testen", db.Workbooks.Last().Subtitle);
+		}
+
+		[Fact]
+		public async Task TestUpdatePage()
+		{
+			var controller = new HomeController(null);
+			var db = DatabaseService.Connect(TestsFixture.ConnectionString);
+
+			var p = new PageModel
+			{
+				Description = "new",
+				PageNumber = 11
+			};
+
+			var result = await controller.UpdatePage(db, new StorageMock(), 1, p);
+
+			Assert.Equal("new", db.FindPageWithRecording(1).Description);
+			Assert.Equal(11, db.FindPageWithRecording(1).Number);
+		}
+
+		[Fact]
+		public async Task TestCreatePage()
+		{
+			var controller = new HomeController(null);
+			var db = DatabaseService.Connect(TestsFixture.ConnectionString);
+
+			var p = new PageModel
+			{
+				Description = "new",
+				PageNumber = 11,
+				WorkbookID = 1,
+				UploadedFile = new FileMock()
+			};
+
+			var result = await controller.CreatePage(db, new StorageMock(), p);
+
+			var w = db.FindWorkbookWithReferences(1);
+			Assert.Equal(5, db.Pages.Count());
+
+			foreach (var page in w.Pages)
+			{
+				if (page.Id != 1 && page.Id != 2)
+				{
+					Assert.Equal("new", page.Description);
+					Assert.Equal(11, page.Number);
+					break;
+				}
+			}
 		}
 	}
 }
