60a085f4971d7b887b2f04752758e695597a6302
Jan Göbel <jan.goebel.home@gmail.com>
2017-02-15 20:20:13 +0100

    fix iOS reachability bug

--- Lingvo/Solution/src/MobileApp.iOS/Services/NetworkService.cs
+++ Lingvo/Solution/src/MobileApp.iOS/Services/NetworkService.cs
@@ -13,66 +13,93 @@ namespace Lingvo.MobileApp.iOS.Services
 {
     public class NetworkService : INetworkService
     {
-        static NetworkReachability defaultRouteReachability;
+		public enum NetworkStatus
+		{
+			NotReachable,
+			ReachableViaCarrierDataNetwork,
+			ReachableViaWiFiNetwork
+		}
 
-        public enum NetworkStatus
-        {
-            NotReachable,
-            ReachableViaCarrierDataNetwork,
-            ReachableViaWiFiNetwork
-        }
+		/// <summary>
+		/// The default route reachability
+		/// </summary>
+		private static NetworkReachability defaultRouteReachability;
 
-        public static event EventHandler ReachabilityChanged;
+		/// <summary>
+		/// Checks if network is reachable without requiring connection.
+		/// </summary>
+		/// <param name="flags">The reachability flags.</param>
+		/// <returns>True if reachable, false if connection is required.</returns>
+		public static bool IsReachableWithoutRequiringConnection(NetworkReachabilityFlags flags)
+		{
+			return (flags & NetworkReachabilityFlags.Reachable) != 0
+				   && (((flags & NetworkReachabilityFlags.IsWWAN) != 0)
+					   || (flags & NetworkReachabilityFlags.ConnectionRequired) == 0);
+		}
 
-        static void OnChange(NetworkReachabilityFlags flags)
-        {
-            ReachabilityChanged?.Invoke(null, EventArgs.Empty);
-        }
 
-        public bool IsWifiConnected()
-        {
-            return InternetConnectionStatus() == NetworkStatus.ReachableViaWiFiNetwork;
-        }
 
-        static NetworkStatus InternetConnectionStatus()
-        {
-            NetworkReachabilityFlags flags;
-            bool defaultNetworkAvailable = IsNetworkAvailable(out flags);
+		/// <summary>
+		/// The reachability changed event.
+		/// </summary>
+		/// <remarks>Raised every time there is an interesting reachable event,
+		/// we do not even pass the info as to what changed, and
+		/// we lump all three status we probe into one.</remarks>
+		public static event EventHandler ReachabilityChanged;
 
-            if (defaultNetworkAvailable && ((flags & NetworkReachabilityFlags.IsDirect) != 0))
-                return NetworkStatus.NotReachable;
 
-            if ((flags & NetworkReachabilityFlags.IsWWAN) != 0)
-                return NetworkStatus.ReachableViaCarrierDataNetwork;
+		/// <summary>
+		/// The internet connection status.
+		/// </summary>
+		/// <returns>The <see cref="NetworkStatus" />.</returns>
+		public static NetworkStatus InternetConnectionStatus()
+		{
+			NetworkReachabilityFlags flags;
 
-            if (flags == 0)
-                return NetworkStatus.NotReachable;
+			if ((IsNetworkAvailable(out flags) && ((flags & NetworkReachabilityFlags.IsDirect) != 0)) || flags == 0)
+			{
+				return NetworkStatus.NotReachable;
+			}
 
-            return NetworkStatus.ReachableViaWiFiNetwork;
-        }
+			return (flags & NetworkReachabilityFlags.IsWWAN) != 0
+					   ? NetworkStatus.ReachableViaCarrierDataNetwork
+					   : NetworkStatus.ReachableViaWiFiNetwork;
+		}
 
-        static bool IsNetworkAvailable(out NetworkReachabilityFlags flags)
-        {
-            if (defaultRouteReachability == null)
-            {
-                var ipAddress = new IPAddress(0);
-                defaultRouteReachability = new NetworkReachability(ipAddress.MapToIPv6());
-                defaultRouteReachability.SetNotification(OnChange);
-                defaultRouteReachability.Schedule(CFRunLoop.Current, CFRunLoop.ModeDefault);
-            }
-            return defaultRouteReachability.TryGetFlags(out flags) && IsReachableWithoutRequiringConnection(flags);
-        }
+	
 
-        static bool IsReachableWithoutRequiringConnection(NetworkReachabilityFlags flags)
-        {
-            // Is it reachable with the current network configuration?
-            bool isReachable = (flags & NetworkReachabilityFlags.Reachable) != 0;
+		/// <summary>
+		/// Called when [change].
+		/// </summary>
+		/// <param name="flags">The flags.</param>
+		private static void OnChange(NetworkReachabilityFlags flags)
+		{
+			var h = ReachabilityChanged;
+			if (h != null)
+			{
+				h(null, EventArgs.Empty);
+			}
+		}
 
-            // Do we need a connection to reach it?
-            bool noConnectionRequired = (flags & NetworkReachabilityFlags.ConnectionRequired) == 0
-                || (flags & NetworkReachabilityFlags.IsWWAN) != 0;
+		/// <summary>
+		/// Returns network reachability flags and network availability.
+		/// </summary>
+		/// <param name="flags">The network reachability flags.</param>
+		/// <returns>True if network is available, otherwise false.</returns>
+		public static bool IsNetworkAvailable(out NetworkReachabilityFlags flags)
+		{
+			if (defaultRouteReachability == null)
+			{
+				defaultRouteReachability = new NetworkReachability(new IPAddress(0));
+				defaultRouteReachability.SetNotification(OnChange);
+				defaultRouteReachability.Schedule(CFRunLoop.Current, CFRunLoop.ModeDefault);
+			}
 
-            return isReachable && noConnectionRequired;
-        }
-    }
+			return defaultRouteReachability.TryGetFlags(out flags) && IsReachableWithoutRequiringConnection(flags);
+		}
+		public bool IsWifiConnected()
+		{
+			return InternetConnectionStatus() == NetworkStatus.ReachableViaWiFiNetwork;
+		}
+	}
 }
