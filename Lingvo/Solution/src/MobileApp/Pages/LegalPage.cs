9e8a327a67478c8a96dfcc412a8a6cac5fd9da14
Philip Lenzen <plenzen@gmx.net>
2017-02-19 23:24:38 +0100

    Added LegalPage

---
+++ Lingvo/Solution/src/MobileApp/Pages/LegalPage.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace Lingvo.MobileApp.Pages
+{
+    public class LegalPage : ContentPage
+    {
+        public LegalPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = ((Span)App.Current.Resources["legal_content"]).Text }
+                }
+            };
+        }
+    }
+}
