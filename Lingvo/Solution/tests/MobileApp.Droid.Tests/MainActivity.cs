166d4bac85e4280ecb0839a8f3e59f4cdf57d1a4
Philip Lenzen <plenzen@gmx.net>
2017-02-20 20:49:31 +0100

    Android Unit tests

---
+++ Lingvo/Solution/tests/MobileApp.Droid.Tests/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Android.App;
+using Android.OS;
+using Xamarin.Android.NUnitLite;
+
+namespace MobileApp.Droid.Tests
+{
+    [Activity(Label = "MobileApp.Droid.Tests", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : TestSuiteActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            AddTest(Assembly.GetExecutingAssembly());
+            
+            base.OnCreate(bundle);
+        }
+    }
+}
+
