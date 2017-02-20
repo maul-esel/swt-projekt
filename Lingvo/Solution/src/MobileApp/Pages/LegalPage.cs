4cb1c71ea7edc60b448a5e12c5dd7cfcb1a3ab3c
Philip Lenzen <plenzen@gmx.net>
2017-02-20 16:42:32 +0100

    Added real impressum

--- Lingvo/Solution/src/MobileApp/Pages/LegalPage.cs
+++ Lingvo/Solution/src/MobileApp/Pages/LegalPage.cs
@@ -6,10 +6,71 @@ namespace Lingvo.MobileApp.Pages
     {
         public LegalPage()
         {
+            Title = ((Span)App.Current.Resources["label_legal"]).Text;
+
+            double fontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label));
+
             Content = new StackLayout
             {
+                Padding = new Thickness(15, 15),
                 Children = {
-                    new Label { Text = ((Span)App.Current.Resources["legal_content"]).Text }
+                    new Label {
+                        Text = ((Span)App.Current.Resources["legal_university"]).Text,
+                        FontAttributes = FontAttributes.Bold,
+                        FontSize = fontSize
+                    },
+                    new Label { Text = ((Span)App.Current.Resources["legal_institute"]).Text,
+                        FontSize = fontSize
+                    },
+                    new Label {
+                        Text = ((Span)App.Current.Resources["legal_link"]).Text,
+                        FontSize = fontSize
+                    },
+                    new Label {
+                        Margin = new Thickness(0, 10),
+                        Text = ((Span)App.Current.Resources["legal_partners"]).Text,
+                        FontAttributes = FontAttributes.Bold,
+                        FontSize = fontSize
+                    },
+                    new Label {
+                        Text = ((Span)App.Current.Resources["legal_maiborn"]).Text,
+                        FontSize = fontSize
+                    },
+                    new Label {
+                        Text = ((Span)App.Current.Resources["legal_tauschring"]).Text,
+                        FontSize = fontSize
+                    },
+                    new Label {
+                        Margin = new Thickness(0, 10),
+                        Text = ((Span)App.Current.Resources["legal_members"]).Text,
+                        FontAttributes = FontAttributes.Bold,
+                        FontSize = fontSize
+                    },
+                    new Label {
+                        Text = ((Span)App.Current.Resources["legal_jan"]).Text,
+                        FontSize = fontSize
+                    },
+                    new Label {
+                        Text = ((Span)App.Current.Resources["legal_dominik"]).Text,
+                        FontSize = fontSize
+                    },
+                    new Label {
+                        Text = ((Span)App.Current.Resources["legal_philip"]).Text,
+                        FontSize = fontSize
+                    },
+                    new Label {
+                        Text = ((Span)App.Current.Resources["legal_katja"]).Text,
+                        FontSize = fontSize
+                    },
+                    new Label {
+                        Text = ((Span)App.Current.Resources["legal_ralph"]).Text,
+                        FontSize = fontSize
+                    },
+                    new Label {
+                        Margin = new Thickness(0, 10),
+                        Text = ((Span)App.Current.Resources["legal_elite"]).Text,
+                        FontSize = fontSize
+                    }
                 }
             };
         }
