8f2977363ab9155682507c9cfa0d8ad4e078aae9
maul.esel <maul.esel@go4more.de>
2017-02-22 19:15:13 +0100

    comment & simplify App.xaml.cs

--- Lingvo/Solution/src/MobileApp/App.xaml.cs
+++ Lingvo/Solution/src/MobileApp/App.xaml.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using System.IO;
-using System.Reflection;
-
 using Xamarin.Forms;
-
 
 namespace Lingvo.MobileApp
 {
-	using Common.Entities;
 	using Services;
 
+	/// <summary>
+	/// The core application class for the entire app.
+	/// </summary>
 	public partial class App : Application
 	{
 		const string databasePath = "lingvo.sqlite";
 
+		/// <summary>
+		/// The <see cref="DatabaseService"/> instance used to access the SQLite database.
+		/// </summary>
 		public static DatabaseService Database { get; private set; }
 
 		private static void SetupDatabaseConnection()
@@ -26,27 +28,12 @@ namespace Lingvo.MobileApp
 			}
 		}
 
-		public App ()
+		public App()
 		{
 			SetupDatabaseConnection();
 			InitializeComponent();
 
 			MainPage = new MobileApp.Pages.MainPage();
-		}
-
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
-
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
-
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
 		}
 	}
 }
